88ac84dfc824a439e96d0324e9fd6c94cdd52fd4
smjde-iul <smjde@iscte-iul.pt>
2018-03-17 01:24:33 +0000

    Actualização JFrame - jtextfield, jbutton e jtextarea

--- Gui_Optimizador.java
+++ Gui_Optimizador.java
@@ -4,35 +4,52 @@ import java.awt.Dimension;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Gui_Optimizador {
 
+	
 	JFrame frame = new JFrame();
 	JButton botaoIniciaOptimizacao = new JButton();
 	JPanel painelBotoes = new JPanel();
-	JTextField texto = new JTextField();
-
-
+	JPanel painelNomeProblema = new JPanel();
+	JPanel painelDescricaoProblema = new JPanel();
+	JTextField nomeProblema = new JTextField(40);
+	JTextArea descricaoProblema = new JTextArea(20,20);
 
 
 	//Definir a interface Gráfica com o utilizador
 	public Gui_Optimizador() {
 		
-		frame.setName("Optimizador");
-		frame.add(painelBotoes, BorderLayout.SOUTH);
+		frame.setName("Optimizer");
+		
+		addContent();
 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocation(50, 50);
+		frame.setResizable(false);
 		frame.setPreferredSize(new Dimension(600, 400));
 		frame.pack();
-
 
 	}
 	
 	
 	public void open() {
 		frame.setVisible(true);
+	}
+	
+	public void addContent() {
+		nomeProblema.setText("Introduza o nome do problema.");
+		botaoIniciaOptimizacao.setText("Start!");
+		painelNomeProblema.add(nomeProblema);
+		descricaoProblema.setText("Introduza a descrição do problema.");
+		painelDescricaoProblema.add(descricaoProblema);
+		painelBotoes.add(botaoIniciaOptimizacao);
+		frame.add(painelNomeProblema, BorderLayout.NORTH);
+		frame.add(painelBotoes, BorderLayout.SOUTH);
+		frame.add(painelDescricaoProblema, BorderLayout.WEST);
+		
 	}
 
 
